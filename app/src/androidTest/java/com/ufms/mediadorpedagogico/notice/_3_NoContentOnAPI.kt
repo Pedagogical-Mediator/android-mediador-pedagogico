9fe20829aa2cf298da9333cda0c89993b272af40
William <william@jera.com.br>
2019-09-10 09:28:57 -0400

    Espresso tests for notice list

---
+++ app/src/androidTest/java/com/ufms/mediadorpedagogico/notice/_3_NoContentOnAPI.kt
@@ -0,0 +1,72 @@
+package com.ufms.mediadorpedagogico.notice
+
+import android.content.Context.WIFI_SERVICE
+import android.net.wifi.WifiManager
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.runner.AndroidJUnit4
+import com.ufms.mediadorpedagogico.R
+import junit.framework.AssertionFailedError
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.TypeSafeMatcher
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+open class _3_NoContentOnAPI : NoticeInstrumental() {
+
+    @Before
+    fun setNoInternet() {
+        val wifiManager = activityRule.activity.getSystemService(WIFI_SERVICE) as WifiManager
+        wifiManager.isWifiEnabled = false
+    }
+
+    @Test
+    fun noContentOnAPI() {
+        try {
+            Thread.sleep(1000)
+            onView(withText(activityRule.activity.getString(R.string.global_try_again))).check(matches(isDisplayed()))
+            Assert.assertTrue(true)
+        } catch (e: AssertionFailedError) {
+            Assert.assertTrue(false)
+        }
+    }
+
+    @After
+    fun setInternet() {
+        val wifiManager = activityRule.activity.getSystemService(WIFI_SERVICE) as WifiManager
+        wifiManager.isWifiEnabled = true
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
