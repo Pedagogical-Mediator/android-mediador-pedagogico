0c0a39eafcf8e7810ebf9362765027eb687e13a2
William <william@jera.com.br>
2019-09-30 11:15:52 -0400

    Landing screen

---
+++ app/src/main/java/com/ufms/mediadorpedagogico/presentation/login/LoginNavData.kt
@@ -0,0 +1,12 @@
+package com.ufms.mediadorpedagogico.presentation.login
+
+import android.content.Context
+import android.content.Intent
+import com.ufms.mediadorpedagogico.presentation.main.MainActivity
+import com.ufms.mediadorpedagogico.presentation.util.structure.navigation.NavData
+
+class LoginNavData : NavData {
+    override fun createIntent(context: Context): Intent {
+        return LoginActivity.createIntent(context)
+    }
+}
